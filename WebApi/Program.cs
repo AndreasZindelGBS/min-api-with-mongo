631d31bd477c41e4e9c0c2fb2936e5b375ba40c0
AndreasZindelGBS <andreas.zindel@edu.gbssg.ch>
2026-05-27 08:49:19 +0200

    lösungen aufgabe 2

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,6 +1,29 @@
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
+var movieDatabaseConfigSection = builder.Configuration.GetSection("DatabaseSettings");
+builder.Services.Configure<DatabaseSettings>(movieDatabaseConfigSection);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return "Minimal API Version 1.0";
+});
+
+app.MapGet("/check", (IOptions<DatabaseSettings> options) =>
+{
+    try
+    {
+        var mongoDbConnectionString = options.Value.ConnectionString;
+        var client = new MongoClient(mongoDbConnectionString);
+        var databases = client.ListDatabaseNames().ToList();
+        return $"Datenbanken: {string.Join(", ", databases)}";
+    }
+    catch (Exception ex)
+    {
+        return $"Fehler: {ex.Message}";
+    }
+});
 
 app.Run();
