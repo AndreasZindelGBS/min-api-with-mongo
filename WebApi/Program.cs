640ae192e57ce4ff041d30cdd3313702184b8d09
AndreasZindelGBS <andreas.zindel@edu.gbssg.ch>
2026-05-27 10:45:20 +0200

    lösungen aufgabe 3

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -2,8 +2,10 @@ using MongoDB.Driver;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-var movieDatabaseConfigSection = builder.Configuration.GetSection("DatabaseSettings");
-builder.Services.Configure<DatabaseSettings>(movieDatabaseConfigSection);
+
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("DatabaseSettings")
+);
 
 var app = builder.Build();
 
@@ -16,47 +18,42 @@ app.MapGet("/check", (IOptions<DatabaseSettings> options) =>
 {
     try
     {
-        var mongoDbConnectionString = options.Value.ConnectionString;
-        var client = new MongoClient(mongoDbConnectionString);
+        var client = new MongoClient(options.Value.ConnectionString);
         var databases = client.ListDatabaseNames().ToList();
-        return $"Datenbanken: {string.Join(", ", databases)}";
+
+        return Results.Ok(databases);
     }
     catch (Exception ex)
     {
-        return $"Fehler: {ex.Message}";
+        return Results.Problem(ex.Message);
     }
 });
 
-
-// Insert Movie
 app.MapPost("/api/movies", (Movie movie) =>
 {
-    throw new NotImplementedException();
+    movie.Id ??= Guid.NewGuid().ToString();
+    return Results.Created($"/api/movies/{movie.Id}", movie);
 });
 
-// Get all Movies
 app.MapGet("/api/movies", () =>
 {
-    throw new NotImplementedException();
+    return Results.Ok(new List<Movie>());
 });
 
-// Get Movie by id
 app.MapGet("/api/movies/{id}", (string id) =>
 {
-    throw new NotImplementedException();
+    return Results.Ok(new Movie { Id = id });
 });
 
-// Update Movie
 app.MapPut("/api/movies/{id}", (string id, Movie movie) =>
 {
-    throw new NotImplementedException();
+    movie.Id = id;
+    return Results.Ok(movie);
 });
 
-// Delete Movie
 app.MapDelete("/api/movies/{id}", (string id) =>
 {
-    throw new NotImplementedException();
+    return Results.Ok();
 });
 
-// WICHTIG: ganz nach unten verschoben
 app.Run();
